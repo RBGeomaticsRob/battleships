4cca5744c20a115a5ca57c8da5a1ddf9d2399579
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-03-24 15:48:44 +0000

    Total Reset

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,11 +1,1 @@
-require 'cell'
-describe Cell do
-  it 'starts empty' do
-    expect(subject.full).not_to be
-  end
 
-  it 'becomes full when shot' do
-    subject.shot
-    expect(subject.full).to be
-  end
-end
