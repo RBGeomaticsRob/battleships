33c570d1ae86b866ee26f168abfbf43a027f609c
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-03-24 14:56:22 +0000

    Refactoring of Ship

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,10 +1,10 @@
 require 'ship'
 
 describe Ship do
-  let(:ship_size) { subject.size = 1 }
+  let(:set_ship_size) { subject.size = 1 }
   let(:sink_a_ship) { subject.size.times { subject.hit } }
   it 'knows its sizes' do
-    ship_size
+    set_ship_size
     expect(subject.size).to be_a Integer
   end
   it 'does not receive a string' do
@@ -12,7 +12,7 @@ describe Ship do
     'Requires an array'
   end
   it 'it can set the co-ordinates' do
-    ship_size
+    set_ship_size
     subject.location_coords!([[:a, 1]])
     expect(subject.coords).to eq [[:a, 1]]
   end
@@ -21,17 +21,17 @@ describe Ship do
     'Requires an array'
   end
   it 'it can set the co-ordinates' do
-    ship_size
+    set_ship_size
     subject.location_coords!([[:a, 1]])
     expect(subject.coords).to eq [[:a, 1]]
   end
   it 'sinks when all co-ords have been #hit' do
-    ship_size
+    set_ship_size
     sink_a_ship
     expect(subject).to be_sunk
   end
   it 'cannot be hit when sunk' do
-    ship_size
+    set_ship_size
     sink_a_ship
     expect { subject.hit }.to raise_error 'Ship Already Sunk'
   end
